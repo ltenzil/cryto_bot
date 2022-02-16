65baaae3569351feb15146eb45328fa4d87f5caf
ltenzil <l.tenzil@gmail.com>
2022-02-17 03:46:28 +0530

    Handling postback

--- lib/crypto_bot_web/controllers/chat_controller.ex
+++ lib/crypto_bot_web/controllers/chat_controller.ex
@@ -18,10 +18,11 @@ defmodule CryptoBotWeb.ChatController do
   def incoming_message(conn, params) do
     if (params["object"] == "page") do
       Enum.each(params["entry"], fn entry ->
-        event = Map.get(entry, "messaging") |> Enum.at(0)
-        %{"message" => %{"text" => text}, "sender" => %{"id" => sender_psid}} = event
-        IO.inspect("Input from user #{text}")
-        send_message(sender_psid, FbMessenger.reply_for(text))
+        IO.inspect(params)
+        event = Map.get(entry, "messaging")
+        |> Enum.at(0)
+        |> handle_event
+        send_message(event[:sender_psid], event[:text])
       end)
       send_resp(conn, 200, "EVENT_RECEIVED")
     else
@@ -29,8 +30,17 @@ defmodule CryptoBotWeb.ChatController do
     end
   end
 
+  defp handle_event(%{"postback" => postback, "recipient" => recipient, "sender" => sender} = event) do    
+    %{ text: postback["payload"], sender_psid: sender["id"], recipient_id: recipient["id"] }
+  end
+
+  defp handle_event(%{"message" => message, "sender" => sender} = event) do
+    %{ text: message["text"], sender_psid: sender["id"], recipient_id: nil }
+  end
+
   defp send_message(psid, message) do
-    FbApi.send_msg(psid, message)
+    reply = FbMessenger.reply_for(message)
+    FbApi.send_msg(psid, reply)
   end
 
 end
