6a0b1f1833012dc71966f29788a41f0664dfc4f6
ltenzil <l.tenzil@gmail.com>
2022-02-17 11:42:45 +0530

    Help text modified similar to other request

--- lib/crypto_bot/fb_messenger.ex
+++ lib/crypto_bot/fb_messenger.ex
@@ -2,14 +2,6 @@ defmodule CryptoBot.FbMessenger do
 
   alias CryptoBot.CoinGeckoApi
   
-  def received_msg(input) do
-
-  end
-
-  def send_msg(message) do
-
-  end
-
   def reply_for(term) do
     term
     |> analyse_input
@@ -34,7 +26,6 @@ defmodule CryptoBot.FbMessenger do
 
   def fetch_data(term), do: help_text
 
-
   def call_apis(type, term) do
     case String.downcase(type) do
       "price" -> CoinGeckoApi.info(term)
@@ -49,24 +40,58 @@ defmodule CryptoBot.FbMessenger do
   end
 
   def format_data("top", data) do
-    coin_batches = Enum.map(data, fn coin ->
-      %{
-        # content_type: "text", for quick replies
-        type: "postback", # for buttons
-        title: coin["name"],
-        payload: "price:#{String.downcase(coin["id"])}"
-      }
-    end) |> Enum.chunk_every(3)
-    # quick_replies(coin_batches, "Top coins:")
-    elements = Enum.map(coin_batches, fn coins ->
-      %{ title: "Top Coins:", buttons: coins }
-    end)
-    elements
+    Enum.map(data, &(buttons(&1))) 
+    |> Enum.chunk_every(3)
+    |> Enum.map(&(element_format(&1)))
     |> attachment_format
     |> attachment_msg
   end
 
-  def attachment_format(elements) do
+  def format_data("price", data) do
+    coin_name = data["name"]
+    market_cap_rank = data["market_cap_rank"]
+    market_data = data["market_data"]
+    price = market_data["current_price"]["usd"]
+    price_change_24h = market_data["price_change_24h"]    
+    change_percentage_24h = market_data["price_change_percentage_24h"]
+    change_percentage_7d = market_data["price_change_percentage_7d"]
+    change_percentage_14d = market_data["price_change_percentage_14d"]
+     
+    "#{coin_name}, \n current price: #{price} $
+      market cap rank: #{market_cap_rank}
+      24h $ change: #{price_change_24h} $
+      24h % change: #{change_percentage_24h} % 
+      7d % change: #{change_percentage_7d} %
+      14d % change: #{change_percentage_14d} %"
+    |> text_msg
+  end
+
+  def format_data(:error, msg) do
+    msg |> text_msg
+  end
+
+  def format_data(_type, _data) do
+    help_text
+  end
+
+  def help_text do
+    title = "Our features are restricted to few keywords:"
+    top_ten = %{"name" => "top:10", "payload" => "top:10" } |> buttons_menu
+    coin_price = %{"name" => "price:iotex", "payload" => "price:iotex" } |> buttons_menu
+    coin_market = %{"name" => "market:ripple", "payload" => "market:ripple" } |> buttons_menu
+    buttons = [top_ten, coin_price, coin_market]
+    element_format(buttons, title)
+    |> attachment_format
+    |> attachment_msg
+  end
+
+  def fetch_user_info(psid) do
+    access_token = Application.get_env(:crypto_bot, :access_token)
+    "https://graph.facebook.com/#{psid}?fields=first_name,last_name&access_token=#{access_token}"
+
+  end
+
+  defp attachment_format(elements) do
     %{
       type: "template",
       payload: %{
@@ -75,47 +100,34 @@ defmodule CryptoBot.FbMessenger do
       }
     }
   end
- 
-  def format_data("price", data) do
-    coin_name = data["name"]
-    market_cap_rank = data["market_cap_rank"]
-    market_data = data["market_data"]
-    price = market_data["current_price"]["usd"]
-    max_supply = market_data["max_supply"]
-    total_supply = market_data["total_supply"]
-    price_change_24h = market_data["price_change_24h"]
-    circulating_supply = market_data["circulating_supply"]
-    change_percentage_24h = market_data["price_change_percentage_24h"]
-    change_percentage_7d = market_data["price_change_percentage_7d"]
-    change_percentage_14d = market_data["price_change_percentage_14d"]
-     
-    "#{coin_name}, \n current price: #{price} $
-      market cap rank: #{market_cap_rank} \n total supply: #{total_supply}
-      max supply: #{max_supply} \n 24h $ change: #{price_change_24h} $
-      24h % change: #{change_percentage_24h} % 
-      7d % change: #{change_percentage_7d} %
-      14d % change: #{change_percentage_14d} %"
-    |> text_msg
+
+  defp buttons(coin) do
+    %{      
+      type: "postback",
+      title: coin["name"],
+      payload: "price:#{String.downcase(coin["id"])}"
+    }
   end
 
-  def format_data(_type, _data) do
-    help_text
+  defp buttons_menu(coin) do
+    %{      
+      type: "postback",
+      title: coin["name"],
+      payload: coin["payload"]
+    }
   end
 
-  def help_text do
-    "Our features are restricted to few keywords, 'top five', price:coin_name, market:coin_name
-     example: 
-     top:10
-     price:iotex
-     market:shiba"
-    |> text_msg
+  defp element_format(buttons, title \\ "Top Coins:"), do: %{ title: title, buttons: buttons }
+
+  defp quick_buttons(coin) do
+    # quick_replies(coin_batches, "Top coins:")
+    %{
+      content_type: "text",
+      title: coin["name"],
+      payload: "price:#{String.downcase(coin["id"])}"
+    }
   end
 
-  def fetch_user_info(psid) do
-    access_token = Application.get_env(:crypto_bot, :access_token)
-    "https://graph.facebook.com/#{psid}?fields=first_name,last_name&access_token=#{access_token}"
-
-  end
 
   def text_msg(msg), do: Jason.encode!(%{text: msg})
   def attachment_msg(msg), do: Jason.encode!(%{attachment: msg})
